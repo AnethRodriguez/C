f9c4bc85a40c64cf54f18da0bfb6d33f0235c1d3
AnethRodriguez <78566405+AnethRodriguez@users.noreply.github.com>
2021-02-11 16:01:51 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+//
+//  main.c
+//  chapter 1.2.1
+//
+//  Created by Aneth Rodriguez on 11/02/21.
+//
+
+#include <stdio.h>
+
+//print a celcius - fahrenheit table
+//for celsius = 0, 20,...,300; floating-point version
+
+int main()
+{
+    float celsius, fahr;
+    float lower, upper, step;
+    
+    lower = 0;
+    upper = 300;
+    step = 20;
+    
+    celsius = lower;
+    
+    {
+        printf("celsius fahr\n");
+    }
+    
+    while (celsius <= upper) {
+        fahr = (celsius * 9.0/5.0 + 32.0);
+        printf ("%3.0f %6.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+    
+}
