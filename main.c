ce865efc5cfb484b2ba1b2c5c7bc3e5e5de209ce
AnethRodriguez <78566405+AnethRodriguez@users.noreply.github.com>
2021-02-11 15:55:03 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  exercise 1.1.1.2
+//
+//  Created by Aneth Rodriguez on 11/02/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("hello, world\c");
+    return 0;
+}
