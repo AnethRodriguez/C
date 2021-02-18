1a0c0a1a936895b5a29f1519afd2915c37a5100c
AnethRodriguez <78566405+AnethRodriguez@users.noreply.github.com>
2021-02-18 14:39:05 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Exercise 1.6
+//
+//  Created by Aneth Rodriguez on 15/02/21.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("The value of EOF is %d.\n", EOF);
+    return 0;
+}
