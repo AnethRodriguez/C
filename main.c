916b09462502b093739581c8f064189f7aa1ca63
AnethRodriguez <78566405+AnethRodriguez@users.noreply.github.com>
2021-02-18 14:44:16 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Exercise 1.6
+//
+//  Created by Aneth Rodriguez on 15/02/21.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Please enter a character:\n");
+    printf("The expression \"getchar() != EOF\" is %d.\n", getchar() != EOF);
+    return 0;
+}
