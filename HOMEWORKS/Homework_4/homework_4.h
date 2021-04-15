22aaa7b2d18b8ae604a7cd0c147c9b457aebdf06
AnethRodriguez <78566405+AnethRodriguez@users.noreply.github.com>
2021-04-15 13:59:00 -0500

    Create homework_4.h

---
+++ HOMEWORKS/Homework_4/homework_4.h
@@ -0,0 +1,14 @@
+//
+//  
+//  HOMEWORK4
+//  HOMEWORK4.h
+//  Created by Aneth Rodriguez on 14/04/21.
+//
+
+#define MAX 100
+
+void push(double);
+double pop(void);
+int getop(char[]);
+int getch(void);
+void ungetch(int);
